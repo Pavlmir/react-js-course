0a5d49d2aff8ddc770386f5abc7b4a7d210fab5d
pavlmir <mirprost@gmail.com>
2024-03-03 23:35:55 +0800

    init

---
+++ todo-list/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Timer} from './layout/Timer'
+import {TodoList} from './layout/TodoList'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Timer />
+      <TodoList />
+    </div>  
+  );
+}
+
+export default App;
