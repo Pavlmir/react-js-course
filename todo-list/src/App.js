276727e4beee2b55b3d235597ed8aea7e79c378a
pavlmir <mirprost@gmail.com>
2024-03-04 11:49:52 +0800

    ok

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Timer} from './layout/Timer'
 import {TodoList} from './layout/TodoList'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Timer />
       <TodoList />
     </div>  
   );
